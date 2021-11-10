b6f4c8b200c38d9d3215ddb8844703c138949552
Jonathan <jonathantoledo11@gmail.com>
2021-11-09 20:29:15 -0800

    commit tree e93046a0e0088f80ff8e3aab3f872dcb02165c96

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+let newArray = ['Sally', 'Bob'];
+let newArray2 = ['Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers(drivers) {
+    console.log(drivers);
+    console.log(newArray);
+    return newArray;
+}
+console.log(returnFirstTwoDrivers(newArray));
+
+
+function returnLastTwoDrivers(drivers) {
+    console.log(drivers);
+    console.log(newArray2);
+    return newArray2;
+}
+console.log(returnLastTwoDrivers(drivers));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    return function (fare) { return num * fare };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers) {
+    console.log(drivers);
+    console.log(newArray);
+    console.log(newArray2);
+    return newArray, newArray2;
+}
+ 
+console.log(selectDifferentDrivers(returnFirstTwoDrivers));
+console.log(selectDifferentDrivers(returnLastTwoDrivers));
